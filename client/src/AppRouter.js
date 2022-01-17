9458b91d508505c7e92037ae2db50e0057668ca4
Dhairya Khara <kharadhairya@gmail.com>
2022-01-17 16:19:15 -0500

    added multiple route support in react

---
+++ client/src/AppRouter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Dashboard from './components/Dashboard'
+import AddItem from './components/AddItem'
+import EditItem from './components/EditItem'
+
+class AppRouter extends React.Component {
+    render() {
+        return (
+            // <Dashboard></Dashboard>
+            <BrowserRouter>
+                <div>
+                    {/* <Routes>
+                        <Route path="/" element={<Dashboard/>} exact={true} />
+                        <Route path="/dashboard/create" element = {<AddItem/>} />
+                    </Routes> */}
+                    <Switch>
+                        <Route path="/" component={Dashboard}exact={true} />
+                        <Route path="/dashboard/create" component = {AddItem} />
+                        <Route path="/dashboard/edit/:id" component={EditItem} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default AppRouter
